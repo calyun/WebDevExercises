07c7665571b701b5127a6789bc4ff2ad6fb5b18b
calyun <thisishaoyu@gmail.com>
2017-09-22 00:36:45 -0700

    FreshFilms

---
+++ FreshFilms/README.md
@@ -0,0 +1,6 @@
+# FreshFilms
+
+### Installation
+
+Run in your bash terminal the following command:
+` python3 play.py `
---
+++ FreshFilms/media.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+class Movie:
+    def __init__(self, title, plot, poster_url, trailer_url):
+        self.title = title
+        self.plot = plot
+        self.poster_url = poster_url
+        self.trailer_url = trailer_url
